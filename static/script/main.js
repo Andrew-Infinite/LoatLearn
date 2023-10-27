ec6a182d50be9a773fab8c86fa85599620d7b7b4
Andrew <b07611024@gmail.com>
2023-10-27 17:56:17 +0800

    fix display problem and added button func for validation

--- static/script/main.js
+++ static/script/main.js
@@ -4,6 +4,8 @@
 
 const socket = io();
 const data_output = new Map();
+let ans = [];
+let index2 = 0;
 
 socket.on('connect', () => {
     console.log('Connected to the server');
@@ -21,6 +23,7 @@ socket.on('receive_data', (data) => {
 });
 
 function start() {
+    ans = []
     socket.emit('request_data');
     document.getElementById("start_page").style.display = "none";
     document.getElementById("count_page").style.display = "block";
@@ -33,7 +36,7 @@ function start() {
 function mode_training(){
     document.getElementById("count_page").style.display = "none";
     document.getElementById("train_page").style.display = "block";
-    Data_Output_Train(data_output.get("data_len")[0],data_output.get("interval")[0],() => {
+    Data_Train_Word(data_output.get("data_len")[0],data_output.get("interval")[0],() => {
             document.getElementById("train_page").style.display = "none";
             document.getElementById("train_done_page").style.display = "block";
         },
@@ -42,16 +45,15 @@ function mode_training(){
         document.getElementById('Output_Audio')
     );
 }
+
 function mode_validate(){
     document.getElementById("start_page").style.display = "none";
     document.getElementById("train_page").style.display = "none";
     document.getElementById("train_done_page").style.display = "none";
     document.getElementById("valid_page").style.display = "block";
     document.getElementById("result_page").style.display = "none";
-    Data_Output_Validation(data_output.get("data_len")[0],1000,()=>{
-        document.getElementById("valid_page").style.display = "none";
-        document.getElementById("result_page").style.display = "block";
-    });
+    index2 = data_output.get("data_len")[0];
+    Data_Output_Validation(index2);
 }
 
 function Countdown(second, textbox, callbackfunc) {
@@ -66,35 +68,58 @@ function Countdown(second, textbox, callbackfunc) {
     }, 1000);
 }
 
-function Data_Output_Train(index, interval, callbackfunc, image_box, textbox, audio){
+function Data_Train_Word(index, interval, callbackfunc, image_box, textbox, audio){
     if(index < 0)
     {
         callbackfunc();
         return;
     }
-    image_box.src = `data:image/jpeg;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(data_output.get("training")[data_output.get("data_len")[0]-index].image)))}`;
+    image_box.style.display = "none";
+    textbox.style.display = "block";
+
     textbox.textContent = data_output.get("training")[data_output.get("data_len")[0]-index].word;
     audio.src = `data:audio/mpeg;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(data_output.get("training")[data_output.get("data_len")[0]-index].audio)))}`;
     audio.currentTime = 0;
     audio.play();
 
     setTimeout(function() {
-        Data_Output_Train(index-1, interval, callbackfunc,image_box, textbox, audio)
+        Data_Train_Pict(index, interval, callbackfunc,image_box, textbox, audio)
     }, interval);
 }
 
-function Data_Output_Validation(index, interval, callbackfunc){
+function Data_Train_Pict(index, interval, callbackfunc, image_box, textbox, audio){
     if(index < 0)
     {
         callbackfunc();
         return;
     }
-    document.getElementById('Output_Image2').src = `data:image/jpeg;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(data_output.get("training")[data_output.get("data_len")[0]-index].image)))}`;
-    document.getElementById('btn1').innerText  = data_output.get("validation")[index][0];
-    document.getElementById('btn2').innerText  = data_output.get("validation")[index][1];
-    document.getElementById('btn3').innerText  = data_output.get("validation")[index][2];
+    image_box.style.display = "block";
+    textbox.style.display = "none";
+
+    image_box.src = `data:image/jpeg;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(data_output.get("training")[data_output.get("data_len")[0]-index].image)))}`;
 
     setTimeout(function() {
-        Data_Output_Validation(index-1, interval,callbackfunc)
+        Data_Train_Word(index-1, interval, callbackfunc,image_box, textbox, audio)
     }, interval);
+}
+
+function Data_Output_Validation(index){
+    if(index < 0)
+    {
+        return;
+    }
+    document.getElementById('Output_Image2').src = `data:image/jpeg;base64,${btoa(String.fromCharCode.apply(null, new Uint8Array(data_output.get("training")[data_output.get("data_len")[0]-index].image)))}`;
+    document.getElementById('btn1').innerText  = data_output.get("validation")[data_output.get("data_len")[0]-index][0];
+    document.getElementById('btn2').innerText  = 1;//data_output.get("validation")[data_output.get("data_len")[0]-index][1];
+    document.getElementById('btn3').innerText  = 2;//data_output.get("validation")[data_output.get("data_len")[0]-index][2];
+}
+
+function answer_selection(whoami){
+    ans.push(document.getElementById(whoami).innerText);
+    index2 = index2 - 1;
+    Data_Output_Validation(index2)
+    if(index2 < 0){
+        document.getElementById("valid_page").style.display = "none";
+        document.getElementById("result_page").style.display = "block";
+    }
 }
